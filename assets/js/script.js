f4e15391f0fb5dde9e0b2a763766447262ed405d
Matěj Teplý <81610648+Majkey25@users.noreply.github.com>
2025-03-19 00:16:07 +0100

    update footer content to display the current year and improve responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,25 @@ window.addEventListener('load', () => {
     const menu = document.querySelector('nav ul'); // Select the menu directly
     const footer = document.querySelector('footer');
     const items = Array.from(menu.children);
-  
+
     // Update the footer content
     function updateFooterContent() {
       const footerText = document.getElementById('footer-text');
       const footerLink = document.getElementById('footer-link');
-  
+      const currentYear = new Date().getFullYear(); // Get the current year
+
       if (window.innerWidth >= 500) {
         // PC Version
-        footerText.innerHTML = 'Made by Matěj Teplý &copy; 2025 All rights reserved.';
+        footerText.innerHTML = `Made by Matěj Teplý &copy; ${currentYear} All rights reserved.`;
       } else {
         // Phone Version
-        footerText.innerHTML = 'Made by Matěj Teplý.';
+        footerText.innerHTML = `Made by Matěj Teplý ${currentYear}.`;
       }
     }
-  
+
     window.addEventListener('resize', updateFooterContent);
     updateFooterContent(); // Initialize footer on page load
-    
+
     // Hamburger menu toggle
     document.getElementById('menu-toggle').addEventListener('click', function() {
         menu.classList.toggle('active'); // Toggle the 'active' class on the nav ul
