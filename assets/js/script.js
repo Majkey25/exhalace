83fc1b4b1624d43b2a0d7fbaa5157052c0d1d7c2
Matěj Teplý <81610648+Majkey25@users.noreply.github.com>
2025-03-18 23:43:54 +0100

    refactor footer and mobile menu styles; update footer content dynamically based on screen size

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-document.getElementById('footer-text').innerHTML = `Made by Matěj Teplý &copy; ${new Date().getFullYear()} All rights reserved.`;
+window.addEventListener('load', () => {
+    const menu = document.querySelector('nav ul');
+    const footer = document.querySelector('footer');
+    const items = Array.from(menu.children);
+  
+    // Update the footer content
+    function updateFooterContent() {
+      const footerText = document.getElementById('footer-text');
+      const footerLink = document.getElementById('footer-link');
+  
+      if (window.innerWidth >= 500) {
+        // PC Version
+        footerText.innerHTML = 'Made by Matěj Teplý &copy; 2025 All rights reserved.';
+      } else {
+        // Phone Version
+        footerText.innerHTML = 'Made by Matěj Teplý.';
+      }
+    }
+  
+    window.addEventListener('resize', updateFooterContent);
+    updateFooterContent(); // Inicializace footeru při načtení stránky
+  });
+  
